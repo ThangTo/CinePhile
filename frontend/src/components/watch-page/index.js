8d1b6b3230173905f409262f37ffca36c95a380a
ThangTo <tothang141020@gmail.com>
2025-11-19 20:05:01 +0700

    Fix(server): Remove .env from repo

--- frontend/src/components/watch-page/index.js
+++ frontend/src/components/watch-page/index.js
@@ -1,3 +1,6 @@
-export {default as ActionBar} from './ActionBar';
-export {default as MovieInfoBrief} from './MovieInfoBrief';
-export {default as }
+export { default as ActionBar } from "./ActionBar";
+export { default as MovieInfoBrief } from "./MovieInfoBrief";
+export { default as RatingModal } from "./RatingModal";
+export { default as RatingSidebar } from "./RatingSidebar";
+export { default as Tooltip } from "./Tooltip";
+export { default as VideoPlayer } from "./VideoPlayer";
